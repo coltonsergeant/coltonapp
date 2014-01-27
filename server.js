a0a5499742b6c3ce52a5aa13bac0d24a6ee0e6cf
Colton Sergeant <colton@coltonsergeant.com>
2014-01-26 21:19:00 -0500

    Fixing problem with port for Heroku.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var server = http.createServer (function(request, response)  {
 	response.end('Hello World');
 });
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log('Server is running...');
