83654d751079d8271161fb9607bf409e470281fe
Colton Sergeant <Colton@Coltons-MacBook-Pro.local>
2014-07-09 22:28:24 -0400

    added database stuff

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var ejs = require('ejs');
 var express = require('express');
 var app = express();
+var mongo = require('mongodb').MongoClient;
 app.use('/static', express.static('static'));
 app.engine('.html',ejs.renderFile);
+
+mongo.connect('mongodb://localhost/coltonapp', function(err, db) {
 app.get('/api/data', function(req, res) {
-	res.json(['red', 'blue', 'green', 'yellow']);
+	db.collection('items').find().toArray(function(err, items) {
+		res.json(items);
 });
+	});
 app.get('/*', function(req, res){
   res.render('index.html');
 });
 app.listen(process.env.PORT||3000);
 console.log("Server is running at http://localhost:3000 type control +c to exit")
+});
